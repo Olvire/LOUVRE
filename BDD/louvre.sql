91e8e581cd042de646d17f1af6711b4de0bea19f
Olvire <olvire@hotmail.com>
2017-11-24 11:57:32 +0100

    Mise a jour BDD avec table tarif

--- BDD/louvre.sql
+++ BDD/louvre.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 23 Novembre 2017 à 13:19
+-- Généré le :  Ven 24 Novembre 2017 à 10:56
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -35,6 +35,14 @@ CREATE TABLE `billet` (
   `age` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Contenu de la table `billet`
+--
+
+INSERT INTO `billet` (`id`, `quantite`, `tarif`, `nom`, `prenom`, `age`) VALUES
+(1, '1', '12', 'Dufour', 'jeanloui', '31'),
+(2, '2', '16', 'DUFOUR', 'michel', '30');
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +59,27 @@ CREATE TABLE `reservation` (
   `periodicite` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `datedenaissance` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `date`, `quantite`, `nom`, `prenom`, `email`, `periodicite`, `datedenaissance`) VALUES
+(5, '2017-11-23 00:00:00', '2', 'DUFOUR', 'Michel', 'monmail@yahoo.fr', 'matin', '2012-01-01 00:00:00'),
+(6, '2017-11-24 00:00:00', '2', 'dupont', 'jean', 'monmail@yahoo.fr', 'matin', '2012-01-01 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tarifs`
+--
+
+CREATE TABLE `tarifs` (
+  `normal` decimal(16,0) NOT NULL,
+  `enfant` decimal(8,0) NOT NULL,
+  `senior` decimal(12,0) NOT NULL,
+  `reduit` decimal(10,0) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Index pour les tables exportées
@@ -76,12 +105,12 @@ ALTER TABLE `reservation`
 -- AUTO_INCREMENT pour la table `billet`
 --
 ALTER TABLE `billet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
